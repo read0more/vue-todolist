f7310139141343bee2094a4294198b14c1a7900f
ykpark <read0more@gmail.com>
2022-02-11 15:56:32 +0900

    중복 회원가입 테스트

--- src/services/test/UserRepositoryStub.js
+++ src/services/test/UserRepositoryStub.js
@@ -2,6 +2,10 @@ export default class UserRepositoryStub {
     users = new Map();
 
     async create(user) {
+        if (this.users.has(user.id)) {
+            throw new Error(`${user.id}는 이미 있는 회원`);
+        }
+
         this.users.set(user.id, user);
         return user;
     }
--- src/services/test/userService.test.js
+++ src/services/test/userService.test.js
@@ -7,14 +7,16 @@ describe("userService test", () => {
    *
    * @type {UserService}
    */
-  let userService = null;
-  beforeEach(() => {
-    userService = new UserService(new UserRepositoryStub());
-  });
+  let userService = new UserService(new UserRepositoryStub());
 
   test("create user", async () => {
     const user = new User("yk", 1234);
     const createdUser = await userService.create(user);
     expect(createdUser).toEqual(user);
+  });
+
+  test("trying exists create user", async () => {
+    const user = new User("yk", 1234);
+    await expect(userService.create(user)).rejects.toThrow(`${user.id}는 이미 있는 회원`);
   });
 });
