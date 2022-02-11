a1b2478469d5467a897dd7cb4a0180adeb8e5178
ykpark <read0more@gmail.com>
2022-02-11 15:57:25 +0900

    create describe로 감쌈

--- src/services/test/userService.test.js
+++ src/services/test/userService.test.js
@@ -2,21 +2,24 @@ import UserService from "../UserService.js";
 import UserRepositoryStub from "./UserRepositoryStub";
 import User from "@/entities/User";
 
-describe("userService test", () => {
-  /**
-   *
-   * @type {UserService}
-   */
-  let userService = new UserService(new UserRepositoryStub());
+/**
+ *
+ * @type {UserService}
+ */
+let userService = null;
+describe("userService", () => {
+  userService = new UserService(new UserRepositoryStub());
 
-  test("create user", async () => {
-    const user = new User("yk", 1234);
-    const createdUser = await userService.create(user);
-    expect(createdUser).toEqual(user);
-  });
+  describe('create', () => {
+    test("create user", async () => {
+      const user = new User("yk", 1234);
+      const createdUser = await userService.create(user);
+      expect(createdUser).toEqual(user);
+    });
 
-  test("trying exists create user", async () => {
-    const user = new User("yk", 1234);
-    await expect(userService.create(user)).rejects.toThrow(`${user.id}는 이미 있는 회원`);
+    test("trying exists create user", async () => {
+      const user = new User("yk", 1234);
+      await expect(userService.create(user)).rejects.toThrow(`${user.id}는 이미 있는 회원`);
+    });
   });
 });
