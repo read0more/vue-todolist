fc53d4a538c23878c8ba5ac0e8910e89f24f6ee1
ykpark <read0more@gmail.com>
2022-02-12 19:30:53 +0900

    vuex 기본 구성 및 getter 테스트

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import state from "./user/state.js";
+import getters from "./user/getters.js";
+import actions from "./user/actions.js";
+import mutations from "./user/mutations.js";
+
+export default {
+  namespaced: true,
+  state,
+};
