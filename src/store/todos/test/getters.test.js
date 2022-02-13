047434f4d829d7961e3f8a911fdff7ce6f8c258f
ykpark <read0more@gmail.com>
2022-02-13 22:15:22 +0900

    reformat

--- src/store/todos/test/getters.test.js
+++ src/store/todos/test/getters.test.js
@@ -1,19 +1,19 @@
 import getters from "../getters.js";
 
 describe("todos getter", () => {
-    test("get todos only done", () => {
-        const state = {
-            todos: [
-                {id: 1, done: true},
-                {id: 2, done: false},
-                {id: 3, done: false},
-                {id: 4, done: true},
-            ]
-        };
-        const filtered = getters.doneTodos(state);
-        expect(filtered).toEqual([
-            {id: 1, done: true},
-            {id: 4, done: true}
-        ]);
-    });
+  test("get todos only done", () => {
+    const state = {
+      todos: [
+        { id: 1, done: true },
+        { id: 2, done: false },
+        { id: 3, done: false },
+        { id: 4, done: true },
+      ],
+    };
+    const filtered = getters.doneTodos(state);
+    expect(filtered).toEqual([
+      { id: 1, done: true },
+      { id: 4, done: true },
+    ]);
+  });
 });
