ba2e5ac454cb19b343f41812ce8fc1caea3bba01
ykpark <read0more@gmail.com>
2022-02-14 15:39:54 +0900

    todos getter추가

--- src/store/modules/todos/getters.js
+++ src/store/modules/todos/getters.js
@@ -1,4 +1,7 @@
 export default {
+    todos: (state) => {
+      return state.todos;
+    },
     doneTodos: (state) => {
         return state.todos.filter((todo) => todo.done);
     }
--- src/store/modules/todos/test/getters.test.js
+++ src/store/modules/todos/test/getters.test.js
@@ -11,6 +11,18 @@ describe("todos getter", () => {
         new Todo({ id: 4, content: "내용4", done: true }),
       ],
     };
+    const todos = getters.todos(state);
+    expect(todos).toEqual(state.todos);
+  });
+  test("get todos only done", () => {
+    const state = {
+      todos: [
+        new Todo({ id: 1, content: "내용1", done: true }),
+        new Todo({ id: 2, content: "내용2", done: false }),
+        new Todo({ id: 3, content: "내용3", done: false }),
+        new Todo({ id: 4, content: "내용4", done: true }),
+      ],
+    };
     const filtered = getters.doneTodos(state);
     expect(filtered).toEqual([
       new Todo({ id: 1, content: "내용1", done: true }),
