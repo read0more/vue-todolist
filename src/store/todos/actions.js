bc711619217b1d520eccc456d3cd7007f35af915
ykpark <read0more@gmail.com>
2022-02-14 12:40:40 +0900

    update, delete 테스트 추가

--- src/store/todos/actions.js
+++ src/store/todos/actions.js
@@ -1,13 +1,40 @@
-import { ADD_TODO, UPDATE_TODO, DELETE_TODO } from "./types.js";
+import { INITIAL_TODO, ADD_TODO, UPDATE_TODO, DELETE_TODO } from "./types.js";
 
+// todo: 실제로 todo 관리할 곳에 요청
 export default {
   addTodoToRepository({ commit }) {
     return new Promise((resolve, reject) => {
-      // todo: 실제로 todo 관리할 곳에 요청
       setTimeout(() => {
         commit(ADD_TODO);
         resolve();
-      }, 500);
+      }, 100);
+    });
+  },
+  getAllTodos({ commit }) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const todosFromRepository = [];
+        commit(INITIAL_TODO, todosFromRepository);
+        resolve();
+      }, 100);
+    });
+  },
+
+  updateTodoToRepository({ commit }) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        commit(UPDATE_TODO);
+        resolve();
+      }, 100);
+    });
+  },
+
+  deleteTodoToRepository({ commit }) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        commit(DELETE_TODO);
+        resolve();
+      }, 100);
     });
   },
 };
--- src/store/todos/test/actions.test.js
+++ src/store/todos/test/actions.test.js
@@ -1,11 +1,30 @@
 import actions from "../actions.js";
-import Todo from "@/entities/Todo";
-import { ADD_TODO, UPDATE_TODO, DELETE_TODO } from "../types.js";
+import {INITIAL_TODO, ADD_TODO, UPDATE_TODO, DELETE_TODO} from "../types.js";
 
 describe("todos actions", () => {
+  let commitStub = null;
+  beforeEach(() => {
+    commitStub = jest.fn();
+  });
+
   test("commit addTodo", async () => {
-    const commitStub = jest.fn();
-    await actions.addTodoToRepository({ commit: commitStub });
+    await actions.addTodoToRepository({commit: commitStub});
     expect(commitStub).toHaveBeenCalledWith(ADD_TODO);
+  });
+
+  test("get all todos", async () => {
+    await actions.getAllTodos({commit: commitStub});
+    // todo: 실제로 통신을 하게 되면 빈 배열 하드 코딩 대신 response에 대한 mock으로
+    expect(commitStub).toHaveBeenCalledWith(INITIAL_TODO, []);
+  });
+
+  test("commit updateTodo", async () => {
+    await actions.updateTodoToRepository({commit: commitStub});
+    expect(commitStub).toHaveBeenCalledWith(UPDATE_TODO);
+  });
+
+  test("commit deleteTodo", async () => {
+    await actions.deleteTodoToRepository({commit: commitStub});
+    expect(commitStub).toHaveBeenCalledWith(DELETE_TODO);
   });
 });
