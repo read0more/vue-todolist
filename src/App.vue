2110df7673e977ec3e27548f4b0780f6cf4239d8
ykpark <read0more@gmail.com>
2022-02-14 12:53:39 +0900

    todomvc 스타일 적용

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <header id="header">
+    <h1>todos</h1>
+    <input id="new-todo" autofocus autocomplete="off" placeholder="What needs to be done?">
+  </header>
+  <section id="main">
+    <input id="toggle-all" type="checkbox">
+    <ul id="todo-list">
+      <li class="todo">
+        <div class="view">
+          <input class="toggle" type="checkbox">
+          <label></label>
+          <button class="destroy"></button>
+        </div>
+        <input class="edit" type="text">
+      </li>
+    </ul>
+  </section>
+  <footer id="footer">
+				<span id="todo-count">
+					<strong></strong> 0 left
+				</span>
+    <ul id="filters">
+      <li><a href="#/all">All</a></li>
+      <li><a href="#/active">Active</a></li>
+      <li><a href="#/completed">Completed</a></li>
+    </ul>
+    <button id="clear-completed">
+      Clear completed 0
+    </button>
+  </footer>
 </template>
 
 <style>
